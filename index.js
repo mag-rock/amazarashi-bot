b5c0e275db4e100dc10ab48d443d3d24fce4c50f
y-takahashi <y-takahashi@seekengine.jp>
2024-12-06 16:07:39 +0900

    deploy test: sample index.js

--- index.js
+++ index.js
@@ -1,49 +1,73 @@
 const functions = require('@google-cloud/functions-framework');
-const { postTweet } = require('./twitterApi');
-const { authorize, getSheats } = require('./spreadsheatApi');
+const OAuth = require('oauth-1.0a');
+const axios = require('axios');
+const crypto = require('crypto');
 
 console.log(`Running on Node.js version: ${process.version}`);
 
+// OAuth認証情報を設定
+const consumerKey = process.env.API_KEY;
+const consumerSecret = process.env.API_KEY_SECRET;
+const accessToken = process.env.ACCESS_TOKEN;
+const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
+
+// OAuthインスタンスを初期化
+const oauth = OAuth({
+	consumer: { key: consumerKey, secret: consumerSecret },
+	signature_method: 'HMAC-SHA1',
+	hash_function(baseString, key) {
+		return crypto
+			.createHmac('sha1', key)
+			.update(baseString)
+			.digest('base64');
+	},
+});
+
+// リクエストデータを設定
+const requestData = {
+	url: 'https://api.twitter.com/2/tweets',
+	method: 'POST',
+	data: {
+		text: "My First Post."
+	},
+};
+
+const token = { key: accessToken, secret: accessTokenSecret };
+
+// OAuthヘッダーを生成
+const oauthHeader = oauth.toHeader(oauth.authorize(requestData, token));
+
+
 functions.http('helloHttp', (req, res) => {
-	// Spreadsheatからデータを取得
-	const params = {
-		spreadsheetId: '18sYgADWuSJKYeA7NiCvDaGaSLhovBiTd5KIrC9yLz8E',
-		targetRange: '歌詞一覧!A2:O',
-	};
-	authorize()
-		.then(auth => getSheats(auth, params))
-		.catch(console.error);
+	const param = [
+		`consumerKey: ${consumerKey.substring(0, 4)}`,
+		`consumerSecret: ${consumerSecret.substring(0, 4)}`,
+		`accessToken: ${accessToken.substring(0, 4)}`,
+		`accessTokenSecret: ${accessTokenSecret.substring(0, 4)}`,
+		`url: ${requestData.url}`,
+		`method: ${requestData.method}`,
+		`data: ${requestData.data}`,
+	].join('\n');
+	console.log(param);
 
-	// ツイートするテキスト
-	const text = "My First Post.";
-
-	// 環境変数から認証情報を取得し、オブジェクトとしてまとめる
-	const consumerKey = process.env.API_KEY;
-	const consumerSecret = process.env.API_KEY_SECRET;
-	const accessToken = process.env.ACCESS_TOKEN;
-	const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
-	const credentials = {
-		consumerKey,
-		consumerSecret,
-		accessToken,
-		accessTokenSecret,
-	};
-
-	// postTweet関数にテキストと認証情報を渡す
-	postTweet(text, credentials)
+	// リクエストを送信
+	axios({
+		url: requestData.url,
+		method: requestData.method,
+		data: requestData.data,
+		headers: {
+			...oauthHeader,
+			'Content-Type': 'application/json',
+		},
+	})
 		.then((response) => {
 			console.log('ステータスコード:', response.status);
 			console.log('レスポンスヘッダー:', response.headers);
 			console.log('レスポンスボディ:', response.data);
-			res.send(response.status + ": " + JSON.stringify(response.data));
+			res.send(response.status + ": " + response.data);
 		})
 		.catch((error) => {
-			if (error.response) {
-				console.error('リクエストエラー:', error.response.status, error.response.data);
-				res.status(error.response.status).send(error.response.data);
-			} else {
-				console.error('リクエストエラー:', error.message);
-				res.status(500).send('Error: ' + error.message);
-			}
+			console.error('リクエストエラー:', error);
+			res.send(error.status + ": " + error.data);
 		});
 });
