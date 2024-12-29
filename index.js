e5cdc4ba58d7d9ff99bb04a61fa5276c270d6cc9
y-takahashi <y-takahashi@seekengine.jp>
2024-12-29 20:26:37 +0900

    feat: Can post a tweet and persist the result.

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { nextLevelOf } = require('./quizLogic');
 const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc');
 const timezone = require('dayjs/plugin/timezone');
+const crypto = require("crypto");
+console.log(crypto.randomUUID());
 
 async function allReadTest() {
 	console.log(`Running on Node.js version: ${process.version}`);
@@ -19,17 +21,33 @@ console.log(`Running on Node.js version: ${process.version}`);
 functions.http('helloHttp', async (req, res) => {
 	console.log(`Request recieved.`);
 
+	// Firestoreからドキュメントを読み取り
+	allReadTest();
 
+	// 現在日付を取得
 	dayjs.extend(utc);
 	dayjs.extend(timezone);
-
 	const todayStr = dayjs().tz('Asia/Tokyo').format('YYYY-MM-DD');
 	console.log(`Today: `, todayStr);
+
 	const docData = await getDocumentsCreatedBy('quiz', todayStr);
 	const nextLevel = nextLevelOf(docData.data);
 
-	// Firestoreからドキュメントを読み取り
-	allReadTest();
+	// Postする内容を作成
+	const randomText = crypto.randomUUID;
+	const postText = `${todayStr}\nレベル${nextLevel}\n${randomText}`;
+	console.log(`Post text: ${postText}`);
+
+	// Firestoreにドキュメントを作成
+	if (docData.data === null | docData.data.length === 0) {
+		await createDocument('quiz', todayStr, {
+			origin_post_id: 'value1',
+			date: '2023-10-15',
+			quiz_posts: [{ level: 0, post_id: 'value1' }]
+		});
+		console.log(`Document created.`);
+	}
+
 
 	// Spreadsheatからデータを取得
 	const params = {
@@ -39,9 +57,6 @@ functions.http('helloHttp', async (req, res) => {
 	authorize()
 		.then(auth => getSheats(auth, params))
 		.catch(console.error);
-
-	// ツイートするテキスト
-	const text = "My First Post.";
 
 	// 環境変数から認証情報を取得し、オブジェクトとしてまとめる
 	const consumerKey = process.env.API_KEY;
@@ -56,11 +71,24 @@ functions.http('helloHttp', async (req, res) => {
 	};
 
 	// postTweet関数にテキストと認証情報を渡す
-	postTweet(text, credentials)
-		.then((response) => {
+	postTweet(postText, credentials)
+		.then(async (response) => {
 			console.log('ステータスコード:', response.status);
 			console.log('レスポンスヘッダー:', response.headers);
 			console.log('レスポンスボディ:', response.data);
+			// 投稿結果を永続化
+			if (nextLevel === 0) {
+				const docId = randomText;
+				await createDocument('quiz', docId, {
+					origin_post_id: response.data.data.id,
+					date: todayStr,
+					quiz_posts: [{ level: 0, post_id: response.data.data.id }]
+				});
+			} else {
+				await updateDocument('quiz', docData.data[0].id, {
+					quiz_posts: docData.data[0].push({ level: nextLevel, post_id: response.data.data.id })
+				});
+			}
 			res.send(response.status + ": " + JSON.stringify(response.data));
 		})
 		.catch((error) => {
