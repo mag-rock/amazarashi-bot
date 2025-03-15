e279f4d89e5527a211d2af0fabd7735ef3941d37
y-takahashi <y-takahashi@seekengine.jp>
2025-03-15 21:33:12 +0900

    refactor: delete js files

--- karutaScriptTest.js
+++
@@ -1,9 +0,0 @@
-const { execute } = require('./karutaScript');
-
-console.log(`Running on Node.js version: ${process.version}`);
-
-execute().then(() => {
-	console.log('Success to post tweet.');
-}).catch((error) => {
-	console.error('Error: ' + error.message);
-});
