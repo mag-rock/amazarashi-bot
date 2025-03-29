1f9d1b4cc03b6d2a8f7b6408ca4e3c860ee998a0
mag-rock <y.takahashi.la89@gmail.com>
2025-03-29 21:57:40 +0900

    chore: Jestのキャッシュ設定を追加し、TypeScriptのトランスフォームオプションを更新

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,19 @@ module.exports = {
 	testEnvironment: 'node',
 	testMatch: ['**/tests/**/*.test.js', '**/tests/**/*.test.ts'],
 	transform: {
-		'^.+\\.ts$': 'ts-jest'
+		'^.+\\.ts$': ['ts-jest', {
+			isolatedModules: true,
+			diagnostics: false
+		}]
 	},
 	moduleFileExtensions: ['js', 'ts'],
 	moduleNameMapper: {
 		'^@/(.*)$': '<rootDir>/src/$1'
-	}
+	},
+	cache: true,
+	cacheDirectory: '<rootDir>/.jest-cache',
+	transformIgnorePatterns: [
+		'node_modules/(?!(twitter-text)/)'
+	],
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
